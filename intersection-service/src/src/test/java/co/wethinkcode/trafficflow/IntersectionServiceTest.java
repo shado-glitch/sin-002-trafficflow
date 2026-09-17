c92c7e6e7b073e522ebf9db75ceee7f2bf4ab8a1
ondisjhb025 <ondisjhb025.student.wethinkcode.co.za>
2026-09-17 14:54:24 +0200

    (test: ) IntersectionServiceTest

---
+++ intersection-service/src/src/test/java/co/wethinkcode/trafficflow/IntersectionServiceTest.java
@@ -0,0 +1,13 @@
+package co.wethinkcode.trafficflow;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+public class IntersectionServiceTest {
+    
+}
