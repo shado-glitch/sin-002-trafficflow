d5fc00578fdc0e126e5707de999a93fcfad1072d
ondisjhb025 <ondisjhb025.student.wethinkcode.co.za>
2026-09-17 14:22:41 +0200

    (test: ) levelEightIsValid() , negativeLevelIsRejected(),levelAboveEightIsRejected()

--- congestion-service/src/test/java/co/wethinkcode/trafficflow/CongestionServiceTest.java
+++ congestion-service/src/test/java/co/wethinkcode/trafficflow/CongestionServiceTest.java
@@ -3,6 +3,7 @@ package co.wethinkcode.trafficflow;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CongestionServiceTest {
 
@@ -20,5 +21,34 @@ class CongestionServiceTest {
         service.setCongestionLevel(5);
 
         assertEquals(5, service.getCongestionLevel());
+    }
+
+     @Test
+    void levelEightIsValid() {
+        CongestionServiceApp service = new CongestionServiceApp();
+
+        service.setCongestionLevel(8);
+
+        assertEquals(8, service.getCongestionLevel());
+    }
+
+    @Test
+    void negativeLevelIsRejected() {
+        CongestionServiceApp service = new CongestionServiceApp();
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> service.setCongestionLevel(-1)
+        );
+    }
+
+    @Test
+    void levelAboveEightIsRejected() {
+        CongestionServiceApp service = new CongestionServiceApp();
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> service.setCongestionLevel(9)
+        );
     }
 }
